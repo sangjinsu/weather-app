26fdc482286ee7d444f0a502fe98e69c70624943
jinsuSang <climbplant39@kakao.com>
2021-11-11 00:15:49 +0900

    feat: change request url

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ form.addEventListener('submit', async (event) => {
   try {
     const {
       data: { forecast },
-    } = await axios.post('http://localhost:3000/weather/', {
+    } = await axios.post('/weather/', {
       address,
     })
 
